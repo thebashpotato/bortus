3a1c31787aa1aaf57ff73bd3b2971dc910919048
Matt Williams <matt.k.williams@protonmail.com>
2023-06-18 12:26:02 -0400

    prompts: Update default load prompt

--- bortus/prompts/load.zsh
+++ bortus/prompts/load.zsh
@@ -1,22 +1,29 @@
-case "$BORTUS_PROMPT" in
-default)
+function __load_default_prompt() {
   source "$BORTUS_PROMPT_DIR/default.zsh"
+}
+
+case "$BORTUS_PROMPT" in
+default || "" || " ")
+  __load_default_prompt
   ;;
 starship)
   if has starship && [ "$SHLVL" -eq 1 ]; then
     eval "$(starship init zsh)"
   else
-    source "$BORTUS_PROMPT_DIR/default.zsh"
+    warn "'starship' not found"
+    __load_default_prompt
   fi
   ;;
 oh-my-posh)
   if has oh-my-posh && [ "$SHLVL" -eq 1 ]; then
     eval "$(oh-my-posh init zsh)"
   else
-    source "$BORTUS_PROMPT_DIR/default.zsh"
+    warn "'oh-my-posh' not found"
+    __load_default_prompt
   fi
   ;;
 *)
-  source "$BORTUS_PROMPT_DIR/default.zsh"
+  warn "'$BORTUS_PROMPT' is not a supported prompt name or framework"
+  __load_default_prompt
   ;;
 esac
