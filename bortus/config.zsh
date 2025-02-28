f5124d70c92ac5653d0cb478547871e40eda7116
Matt Williams <matt.k.williams@protonmail.com>
2025-02-27 21:46:24 -0800

    config: Change to a more simple ohmyposh prompt

--- bortus/config.zsh
+++ bortus/config.zsh
@@ -8,8 +8,8 @@
 # Invalid values will be ignored and the default shell will be loaded
 BORTUS_PROMPT="oh-my-posh"
 
-# a using oh-my-posh, set the theme path here
-BORTUS_OH_MY_POSH_THEME="$HOME/.cache/oh-my-posh/themes/stelbent.minimal.omp.json"
+# Using oh-my-posh, set the theme path here
+BORTUS_OH_MY_POSH_THEME="$HOME/.cache/oh-my-posh/themes/json.omp.json"
 
 # BORTUS_REPORTING_TOOL: Accepted values are (neofetch, screenfetch, pfetch, yarsi, etc)
 # Basically any terminal fetch tool will be used by name if it is found on the system.
